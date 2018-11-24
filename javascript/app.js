dfd6d0b03c13c1aeac98a0edeafb556c26a98004
Alex Cho <alexsjcho@gmail.com>
2018-11-24 10:18:31 -0800

    still working on it

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,10 @@
 let friends = ["Joe", "Jack", "Jill","Jamie", "Jason"];
+let songLyrics = "Don\t stop believing ${friends[i]}";
+
+for (let i > 6; i > 0; i++) {
+    console.log(friends);
+} while ( let n < 100; n > 0; n--) {
+    friends.forEach(function(songLyrics){
+    })  
+};
+
