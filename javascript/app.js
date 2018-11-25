ca70fe51e84eefd01945c2254b4f69a8357b9626
Alex Cho <alexsjcho@gmail.com>
2018-11-25 11:31:48 -0800

    got Covalence Alumni Help

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,36 @@
-let friends = ["Joe", "Jack", "Jill", "Jamie", "Jason"];
-let song = ["You are awesome" + friends];
-    for (i = 0; i < friends.length; i++) {
-        console.log(friends[i]);
-     } for (n = 0; n > 100; n--) {
-         song.forEach(function(e){
-        console.log(e);
-     });
-    }
+let friends = [["Joe", "Jack", "Jill", "Jamie", "Jason"], 
+["you are awesome, awesome, awesome!"]];
 
+for (i = 0; i < friends.length; i++) {
+    // for each iteration of this loop the next runs
+    // you can put something here if you want
+    for(n = 0; n>100; n--) {
+    // put what happens in this loop
+    }
+    // things in this place would happen when the second loop finishes
+}
+
+
+
+//Original Code
+// let friends = [["Joe", "Jack", "Jill", "Jamie", "Jason"], 
+// ["you are awesome, awesome, awesome!"]];
+
+//     for (i = 0; i < friends.length; i++) {
+//         for (n = 100; n > 100; n--) { 
+//         }
+//      } 
+//  console.log(friends[i][n]);
+ 
+
+//Tips from Covalence Alumni Justin
+/*
+ for (i = 0; i < friends.length; i++) {
+    // for each iteration of this loop the next runs
+    // you can put something here if you want
+    for(n = 0; n>100; n--) {
+    // put what happens in this loop
+    }
+    // things in this place would happen when the second loop finishes
+}
+*/
